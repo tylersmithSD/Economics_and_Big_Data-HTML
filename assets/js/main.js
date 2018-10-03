9a7cdf51a3549d1eca5bc2dc3dcbe546b6aad0a3
Tyler Smith <43823667+tylersmithSD@users.noreply.github.com>
2018-10-03 17:42:56 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+    /*---------------------------------------------*
+     * Scroll Up Section
+     ---------------------------------------------*/
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200)
+        {
+            $('.scrollup').fadeIn();
+        }
+        else
+        {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+    /*---------------------------------------------*
+     * Scroll Total Navbar
+     ---------------------------------------------*/
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+	
+	 /*---------------------------------------------*
+     * Timeline Section
+     ---------------------------------------------*/
+	
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
